01fc587bfac8e7e714a9b0af8a623bf923e2a676
arjunpadaliya <ap1170@wildcats.unh.edu>
2022-02-18 15:53:53 -0500

    Added Update View

--- ToDo_List/base/urls.py
+++ ToDo_List/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('task-list', TaskList.as_view(), name='task-list'),
     path('task-detail/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
-    path('task-create/', TaskCreate.as_view(), name='task-create')
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update')
 ]
