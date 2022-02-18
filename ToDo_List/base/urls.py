fb3e528bd01b299b0b37073999931682c9c7b717
arjunpadaliya <ap1170@wildcats.unh.edu>
2022-02-18 12:09:41 -0500

    Added Detail Based View

--- ToDo_List/base/urls.py
+++ ToDo_List/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
-    path('task_list', TaskList.as_view(), name='tasks')
+    path('task_list', TaskList.as_view(), name='tasks'),
+    path('task_detail/<int:pk>/', TaskDetail.as_view(), name='task_detail')
 ]
