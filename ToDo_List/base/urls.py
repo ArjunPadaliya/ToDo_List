aaddaa59310594df1905ed306623813ec9d0b43e
arjunpadaliya <ap1170@wildcats.unh.edu>
2022-02-18 15:16:55 -0500

    Added Create View

--- ToDo_List/base/urls.py
+++ ToDo_List/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
-    path('task_list', TaskList.as_view(), name='tasks'),
-    path('task_detail/<int:pk>/', TaskDetail.as_view(), name='task_detail')
+    path('task-list', TaskList.as_view(), name='task-list'),
+    path('task-detail/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('task-create/', TaskCreate.as_view(), name='task-create')
 ]
--- ToDo_List/base/views.py
+++ ToDo_List/base/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
+from django.urls import reverse_lazy
 
 # Create your views here.
 class TaskList(ListView):
@@ -12,3 +14,8 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list')
