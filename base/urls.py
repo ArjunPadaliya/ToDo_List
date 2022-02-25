6c0001649a61fadef8dd43c590f258f023540949
arjunpadaliya <ap1170@wildcats.unh.edu>
2022-02-25 12:53:31 -0500

    Added user login and logout functionality

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
-
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('task-list', TaskList.as_view(), name='task-list'),
     path('task-detail/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>', TaskDelete.as_view(), name='task-delete'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
